27e8e1a44916ef29e2be9ef1a60deff2d6d0cd43
Andy Wang <106629086+awangwang@users.noreply.github.com>
2024-05-12 22:25:55 -0700

    Add files via upload

---
+++ stenosis_yolo.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n-seg.pt")  # load a pretrained model (recommended for training)
+
+# Use the model
+model.train(data="data.yaml", epochs=8, imgsz=512, device='mps')  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
+
+print(metrics)
+# results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+# path = model.export(format="onnx")  # export the model to ONNX format
+
+# model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
